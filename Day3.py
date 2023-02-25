a3b4d5690e8849521216498171d69b8b406cfa07
johyeongseob <johyeongseob@kakao.com>
2023-02-25 19:50:25 +0900

    day3

--- Day3.py
+++ Day3.py
@@ -2,19 +2,18 @@
 
 from collections import Counter
 
-arr=[1,1,2,2]
+arr=[1,1,1,2,2,2]
 cnt = Counter(arr)
-ans = cnt.most_common()
+order = cnt.most_common()
 
-ans1=ans[0][1]
-print(ans)
+ans=order[0][0]
 
 # 여러개 최빈값의 경우 -1
 
 try:
-    if ans[0][1]==ans[1][1]:
-        ans1=-1
+    if order[0][1]==order[1][1]:
+        ans=-1
 except:
-    ans1=ans1
+    ans=ans
 
-print(ans1)
+print(ans)
