33688d28fff3eef42ce15627fd54a969a885dacd
johyeongseob <118985648+johyeongseob@users.noreply.github.com>
2023-02-25 19:39:27 +0900

    프로그래머스 입문 Day3
    #최빈값 구하기

---
+++ Day3.py
@@ -0,0 +1,17 @@
+# 최빈값 구하기
+
+from collections import Counter
+
+arr=[1,1,2,2]
+cnt = Counter(arr)
+ans = cnt.most_common()
+
+ans1=ans[0][1]
+print(ans)
+try:
+    if ans[0][1]==ans[1][1]:
+        ans1=-1
+except:
+    ans1=ans1
+
+print(ans1)
