5499bed6dfdd1e5b704c14c03a2469bf0ad28c24
johyeongseob <johyeongseob@kakao.com>
2023-02-28 21:54:13 +0900

    day4 230228

--- Day4.py
+++ Day4.py
@@ -21,3 +21,19 @@ def solution(n):
 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때, n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 
 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
 """
+
+def solution(n):
+    answer=10
+    for i in range(1,601):
+        if i%6 ==0 and i%n==0:
+            answer=i/6
+            break
+    return answer  
+
+#other solution
+
+def solution(n):
+    answer = 1
+    while 6 * answer % n:
+        answer += 1
+    return answer
