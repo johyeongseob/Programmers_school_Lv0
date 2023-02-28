796476cbc7f021178ad0d92c72a8fb3811598a02
johyeongseob <johyeongseob@kakao.com>
2023-02-28 21:19:54 +0900

    day3

--- Day3.py
+++ Day3.py
@@ -1,19 +1,20 @@
 # 최빈값 구하기
 
+"""최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 
+정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 
+최빈값이 여러 개면 -1을 return 합니다."""
+
 from collections import Counter
 
-arr=[1,1,1,1,2,2,2,2,2]
-cnt = Counter(arr)
-order = cnt.most_common()
-
-ans=order[0][0]
-
-# 여러개 최빈값의 경우 -1
-
-try:
-    if order[0][1]==order[1][1]:
-        ans=-1
-except:
-    ans=ans
-
-print(ans)
+def solution(array):
+    cnt = Counter(array)
+    ans = cnt.most_common()
+    ans1=ans[0][0]
+    
+    try:
+        if ans[0][1]==ans[1][1]:
+            ans1=-1
+    except:
+        ans1=ans1
+    
+    return ans1
