1b192be052688a7a7e21054cf8ebb06d5163f234
johyeongseob <johyeongseob@kakao.com>
2023-03-06 19:35:02 +0900

    day5 230306

--- Day5.py
+++ Day5.py
@@ -3,3 +3,41 @@
 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
 """
 
+def solution(price):
+    answer=0
+    if price >= 500000:
+        answer=price*0.8
+    elif price >= 300000:
+        answer=price*0.9
+    elif price >= 100000:
+        answer=price*0.95
+    else:
+        answer=price
+    
+    return int(answer)
+
+# other solution
+
+def solution(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
+        
+
+"""
+머쓱이는 추운 날에도 아이스 아메리카노만 마십니다.
+아이스 아메리카노는 한잔에 5,500원입니다.
+머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 
+머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 
+순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+"""
+
+def solution(money):
+    cup= money//5500
+    changes=money-cup*5500
+    answer = [cup, changes]
+    return answer
+
+
+
