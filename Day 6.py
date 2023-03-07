929300008955e9f70fcf8f2a3fd03b4ca42bbd6c
johyeongseob <johyeongseob@kakao.com>
2023-03-07 23:17:01 +0900

    day6 230307

--- Day 6.py
+++ Day 6.py
@@ -17,3 +17,18 @@ def solution(my_string):
 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
 """
 
+n = int(input())
+ans=list(range(1, n+1))
+for i in ans:
+    ans2=list(range(1, i+1))
+    for j in ans2:
+        print('*', end='')
+    print()
+        
+# other solution
+
+n = int(input())
+for i in range(1,n+1):
+    print('*'*i)
+
+    
